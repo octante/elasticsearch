3d222fc9373236387ff42765c216e9341113dcf4
Issel Guberna <issel.guberna@gmail.com>
2013-01-08 23:29:45 +0100

    Added type dropdown in query module

--- src/ElasticSearch/ManagementBundle/Controller/QueryController.php
+++ src/ElasticSearch/ManagementBundle/Controller/QueryController.php
@@ -17,9 +17,22 @@ class QueryController extends Controller
 
     public function queryAction (Request $request)
     {
-        $query = $request->request->get('query');
-        $from  = $request->request->get('from');
-        $limit = $request->request->get('limit');
+        $query      = $request->request->get('query');
+        $from       = $request->request->get('from');
+        $index_name = $request->request->get('index_name');
+        $type_name  = $request->request->get('type_name');
+
+        // Limit results to 50
+        $limit  = ($request->request->get('limit') != '' && $request->request->get('limit') < 200) ? $request->request->get('limit') : 50;
+        $fields = array();
+        $page_params = array(
+            'query'                => $query,
+            'tab'                  => 'query',
+            'page_title'           => 'Query Tool',
+            'index_name'           => $index_name,
+            'index_filter_options' => $this->getIndices()
+        );
+
         if ($query != '') {
             $this->aQuery = json_decode($query, true);
 
@@ -28,20 +41,12 @@ class QueryController extends Controller
 
             $exists_fields = (isset($this->aQuery['fields']));
 
-            $request = (new Query_Request(new Elastica_Client(), 'website/_search', 'POST', array(), $this->aQuery))->send()->getData();
-        }
+            $url = '';
+            $url .= ($index_name != '') ? '/' . $index_name : '';
+            $url .= ($type_name != '') ? '/' . $type_name : '';
 
-        $page_params = array(
-            'results'       => null,
-            'query'         => $query,
-            'tab'           => 'query',
-            'page_title'    => 'Query Tool',
-            'from'          => $from,
-            'limit'         => $limit
-        );
+            $request = (new Query_Request(new Elastica_Client(), $url . '/_search', 'POST', array(), $this->aQuery))->send()->getData();
 
-        $fields = array();
-        if ($query != '') {
             $hits         = $request['hits']['hits'];
             $total        = $request['hits']['total'];
             if ($total > 0 && count($hits) > 0) {
@@ -59,17 +64,89 @@ class QueryController extends Controller
                 $fields = array();
             }
 
-            $page_params['total']   = $total;
-            $page_params['results'] = $results;
-            $page_params['fields']  = $fields;
+            $page_params['total']           = $total;
+            $page_params['results']         = $results;
+            $page_params['fields']          = $fields;
+            $page_params['from']            = $from;
+            $page_params['limit']           = $limit;
+            $page_params['indice_name']     = $index_name;
+            $page_params['type_name']       = $type_name;
+
+            $page_params = array_merge($page_params, $this->getIndiceTypes($index_name));
         }
+
         return $this->render('ElasticSearchManagementBundle:Query:query.html.twig', $page_params);
     }
 
+    /**
+     * Ajax request to return types dropdown
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getTypesDropdownAction (Request $request)
+    {
+        $indice_name = $request->query->get('indice_name');
+
+        $page_params = array_merge($page_params, $this->getIndiceTypes($indice_name));
+
+        return $this->render('ElasticSearchManagementBundle:Query:type_dropdown.html.twig', $page_params);
+    }
+
+    /**
+     * Add extraparams to query array, for example skip, limit, etc..
+     *
+     * @param $params
+     */
     private function addExtraParams($params)
     {
         foreach ($params as $field => $value) {
             $this->aQuery[$field] = $value;
         }
+    }
+
+    /**
+     * Get cluster available indices list
+     *
+     * @return array
+     */
+    private function getIndices ()
+    {
+        $cluster_state = (new \Elastica_Cluster(new \Elastica_Client()))->getState();
+        $indices       = array_keys($cluster_state['metadata']['indices']);
+
+        $indice = array();
+        foreach ($indices as $index) {
+            $indice[$index] = $index;
+        }
+        return $indice;
+    }
+
+    /**
+     * Return page params related to type dropdown
+     *
+     * @param $indice_name
+     * @return array
+     */
+    private function getIndiceTypes ($indice_name)
+    {
+        if ('' != $indice_name) {
+            if ($indice_name != '') {
+                $indice_mapping = (new \Elastica_Index(new \Elastica_Client(), $indice_name))->getMapping();
+                foreach ($indice_mapping[$indice_name] as $type => $properties) {
+                    $indice_types[$type] = $type;
+                }
+            } else {
+                $indice_types = array();
+            }
+
+            $page_params = array (
+                'type_filter_options' => $indice_types
+            );
+        } else {
+            $page_params['no_index'] = true;
+        }
+
+        return $page_params;
     }
 }
